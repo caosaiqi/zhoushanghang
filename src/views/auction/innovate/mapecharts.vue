67fe44774021a217632dc791e7bbe6873a74b2e0
曹赛琪 <caosaiqi@yunio>
2020-11-22 13:08:52 +0800

    fe

--- src/views/auction/innovate/mapecharts.vue
+++ src/views/auction/innovate/mapecharts.vue
@@ -3,85 +3,105 @@
 </template>
 <script>
 import "echarts/map/js/china.js";
-import city from "echarts/map/json/china-cities.json";
+import mapData from '@/utils/mapData.js'
+
 export default {
   name: "ECharts",
   components: {},
   data() {
     return {
-      mapData: [
-        { name: "成都", number: 102, count: 399999, good: 2313, bad: 144 },
-        { name: "绵阳", number: 169, count: 1931, good: 1869, bad: 62 },
-        { name: "西安", number: 164, count: 1291, good: 1200, bad: 91 },
-        { name: "长沙", number: 157, count: 2142, good: 2098, bad: 44 },
-        { name: "武汉", number: 256, count: 2331, good: 2070, bad: 261 },
-        { name: "南京", number: 128, count: 2699, good: 2556, bad: 143 },
-        { name: "天津", number: 136, count: 1277, good: 1051, bad: 226 },
-        { name: " 重庆", number: 138, count: 2108, good: 2085, bad: 23 },
-        { name: "杭州", number: 266, count: 2734, good: 2609, bad: 125 },
-        { name: "广州", number: 133, count: 2938, good: 2746, bad: 192 },
-        { name: "青岛", number: 252, count: 1604, good: 1306, bad: 298 },
-        { name: "厦门", number: 270, good: 2639, bad: 246 },
-        { name: " 南昌", number: 211, good: 964, bad: 157 },
-        { name: "贵安", number: 120, good: 2432, bad: 102 },
-        { name: "合肥", number: 125, good: 1540, bad: 41 },
-        { name: "哈尔滨", number: 186, good: 2730, bad: 196 },
-        { name: " 郑州", number: 121, good: 1662, bad: 125 },
-        { name: "无锡", number: 270, good: 2639, bad: 246 },
-        { name: " 苏州", number: 211, good: 964, bad: 157 },
-        { name: "舟山", number: 120, good: 2432, bad: 102 },
-        { name: "太原", number: 125, good: 1540, bad: 41 },
-      ],
+      cityList: [],
+      geoCoordMap: {}
     };
   },
-  mounted() {
+  async created() {
+     await this.getCityData()
     this.chinaConfigure();
   },
   methods: {
-    async lementData() {
-      const  { data  } = await this.$api.element.findAllByAllElement()
-      return data
+    async getCityData () {
+      try {
+        const ret = await this.$api.element.findAllByAllElement()
+        if (ret.data && ret.data.length > 0) {
+          this.cityList = ret.data.map(item => {
+            const { cityName, count} = item
+            const { latitude, longitude} = mapData[cityName]
+            return {
+              count,
+              name: item.cityName,
+              value: [parseFloat(longitude), parseFloat(latitude)]
+            }
+          })
+        }
+        console.log(this.cityList)
+      } catch(err) {
+        throw err
+      }
     },
-    async chinaConfigure() {
+    tooltipStr ({name, items}) {
+      return  `
+          <div
+            style="
+              width: 280px;
+              height: 200px;
+              background: rgba(22, 80, 158, 0.8);
+              border: 1px solid rgba(7, 166, 255, 0.7);
+            "
+          >
+            <div
+              style="
+                width: 100%;
+                height: 40px;
+                line-height: 40px;
+                border-bottom: 2px solid rgba(7, 166, 255, 0.7);
+                padding: 0 20px;
+              "
+            >
+            <i
+              style="
+                display: inline-block;
+                width: 8px;
+                height: 8px;
+                background: #16d6ff;
+                border-radius: 40px;
+              "
+            ></i
+            > ${name}<span style="margin-left: 10px; color: #fff; font-size: 16px"></span>
+          </div>
+          <div style="padding: 20px">
+            ${ 
+               items.map(name => {
+                 return `
+                    <p style="color: #fff; font-size: 12px">
+                      <i
+                        style="
+                          display: inline-block;
+                          width: 10px;
+                          height: 10px;
+                          background: #16d6ff;
+                          border-radius: 40px;
+                          margin: 0 8px;
+                        "
+                      ></i
+                      >${name}<span style="color: #f48225; margin: 0 6px"></span>
+                    </p>
+                 `
+               }).join('|').replace('|', '')
+            }
+          </div>
+        </div>
+      `
+    },
+    chinaConfigure() {
       var _this = this;
       let myChart = this.$echarts.init(this.$refs.platform); //这里是为了获得容器所在位置
-
-      var geoCoordMap = {};
-      var mapName = "china";
-      myChart.showLoading();
-      var mapFeatures = city.features;
-      myChart.hideLoading();
-      mapFeatures.forEach(function (v) {
-        // 地区名称
-        var name = v.properties.name; // 地区经纬度
-        geoCoordMap[name] = v.properties.cp;
-      });
-      var data = [];
-      data = this.mapData;
-      //  console.log(data,geoCoordMap)
-      var max = 480,
-        min = 9; // todo
-      var maxSize4Pin = 100,
-        minSize4Pin = 20;
-      var convertData = function (data) {
-        var res = [];
-        for (var i = 0; i < data.length; i++) {
-          var geoCoord = geoCoordMap[data[i].name];
-          if (geoCoord) {
-            res.push({
-              name: data[i].name,
-              count: data[i].count,
-              value: geoCoord.concat(data[i].value),
-            });
-          }
-        }
-        return res;
-      };
+      var max = 480, min = 9; // todo
+      var maxSize4Pin = 100, minSize4Pin = 20;
       window.onresize = myChart.resize;
       myChart.setOption({
         // 进行相关配置
         title: {
-          text: "创新资源要素分布图",
+          text: "军工产业重点城市",
         },
         backgroundColor: "transparent",
         tooltip: {
@@ -93,51 +113,14 @@ export default {
           enterable: false,
           transitionDuration: 0,
           extraCssText: "z-index:100",
-          formatter: function (params, ticket, callback) {
+          formatter: (params, ticket, callback) => {
             // if (ticket.indexOf('散点') != -1) {
-            let index = params.dataIndex;
-
-            console.log(_this.mapData);
-            let name = params.name;
-            var tipHtml = "";
-            if (_this.mapData[index].count) {
-              tipHtml =
-                '<div style="width:280px;height:200px;background:rgba(22,80,158,0.8);border:1px solid rgba(7,166,255,0.7)">' +
-                '<div style="width:100%;height:40px;line-height:40px;border-bottom:2px solid rgba(7,166,255,0.7);padding:0 20px">' +
-                '<i style="display:inline-block;width:8px;height:8px;background:#16d6ff;border-radius:40px;">' +
-                "</i>" +
-                '<span style="margin-left:10px;color:#fff;font-size:16px;">' +
-                name +
-                "</span>" +
-                "</div>" +
-                '<div style="padding:20px">' +
-                '<p style="color:#fff;font-size:12px;">' +
-                '<i style="display:inline-block;width:10px;height:10px;background:#16d6ff;border-radius:40px;margin:0 8px">' +
-                "</i>" +
-                "军民融合主战区" +
-                '<span style="color:#f48225;margin:0 6px;">';
-              "</span>" + "</div>" + "</div>";
-            } else {
-              tipHtml =
-                '<div style="width:280px;height:200px;background:rgba(22,80,158,0.8);border:1px solid rgba(7,166,255,0.7)">' +
-                '<div style="width:100%;height:40px;line-height:40px;border-bottom:2px solid rgba(7,166,255,0.7);padding:0 20px">' +
-                '<i style="display:inline-block;width:8px;height:8px;background:#16d6ff;border-radius:40px;">' +
-                "</i>" +
-                '<span style="margin-left:10px;color:#fff;font-size:16px;">' +
-                name +
-                "</span>" +
-                "</div>" +
-                '<div style="padding:20px">' +
-                '<p style="color:#fff;font-size:12px;">' +
-                '<i style="display:inline-block;width:10px;height:10px;background:#16d6ff;border-radius:40px;margin:0 8px">' +
-                "</i>" +
-                "军民融合支点城市" +
-                '<span style="color:#f48225;margin:0 6px;">';
-              "</span>" + "</div>" + "</div>";
-            }
-
-            return tipHtml;
-            // }
+            const { data = {} } = params
+            const { items, name } = data
+            if (items && items.length) {
+              return this.tooltipStr(data)
+            } 
+            return null;
           },
         },
         visualMap: {
@@ -153,7 +136,7 @@ export default {
           },
         },
         geo: {
-          map: mapName,
+          map: 'china',
           label: {
             normal: {
               show: false,
@@ -189,7 +172,7 @@ export default {
             name: "散点",
             type: "effectScatter",
             coordinateSystem: "geo",
-            data: convertData(this.mapData),
+            data: this.cityList,
             rippleEffect: {
               //涟漪特效
               period: 4, //动画时间，值越小速度越快
@@ -218,13 +201,7 @@ export default {
             name: "散点",
             type: "effectScatter",
             coordinateSystem: "geo",
-            data: convertData(
-              this.mapData
-                .sort(function (a, b) {
-                  return b.count - a.count;
-                })
-                .slice(0, 5)
-            ),
+            data: this.cityList,
             symbolSize: 8,
             showEffectOn: "render",
             rippleEffect: {
